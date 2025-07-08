761ba7561af88a1f4640941d124184a5022ed1cc
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-07-08 14:45:49 +0200

    add price id

--- shared/price.util.ts
+++ shared/price.util.ts
@@ -4,6 +4,7 @@ export type planType = "free" | "pro" | "ultimate";
 const planMap: Record<priceId, planType> = {
   price_1Ri8kwLuCK4UmVgSsh5T3laq: "pro",
   price_1RiCFZQ6AECzZeTtcdGuhaKb: "pro", // TEST
+  price_1Riak1LuCK4UmVgSstfwkQzG: "pro", // TEST
 };
 
 export const convertPriceIdToPlanType = (priceId: priceId): planType => {
