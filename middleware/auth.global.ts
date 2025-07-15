50253c9add98308f99e3caa1a4d169a0fd2e0e39
Timothy Alcaide <timothy.alcaide@gmail.com>
2025-07-15 07:50:57 +0200

    remove home page redirection

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
   const user = useSupabaseUser();
 
-  const authForbiddenPath = ["/", "/login"];
+  const authForbiddenPath = ["/login"];
 
   if (user.value && authForbiddenPath.includes(to.path)) {
     return navigateTo("/dashboard");
